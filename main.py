2d68bfe5780862cf76eac4b79a11f7f703cfe94b
tomekkoziel <tomasz.koziel66@gmail.com>
2022-11-08 00:48:08 +0100

    data set in csv file and converter to csv file

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import csv    
+
+with open('seeds_dataset.csv', 'w') as csv_file:
+    csv_writer = csv.writer(csv_file, delimiter='\t')
+    csv_writer.writerow(['Area', 'Perimeter', 'Compactness', 'Length of kernel', 'Width of kernel', 'Asymmetry coefficient', 'Length of kernel groove'])
+
+read_file = pd.read_csv('seeds_dataset.txt')
+read_file.to_csv('seeds_dataset.csv', mode="a", index=None)
