d57da56a152983cffd030eb386424cadbd976f9a
tomekkoziel <tomasz.koziel66@gmail.com>
2022-11-08 02:42:10 +0100

    Fixed tabs

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ with open('seeds_dataset.csv', 'w') as csv_file:
     csv_writer.writerow(['Area', 'Perimeter', 'Compactness', 'Length of kernel', 'Width of kernel', 'Asymmetry coefficient', 'Length of kernel groove'])
 
 read_file = pd.read_csv('seeds_dataset.txt')
-read_file.to_csv('seeds_dataset.csv', mode="a", index=None)
+read_file.to_csv('seeds_dataset.csv', sep='\t', mode="a", index=None)
